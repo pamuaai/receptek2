96d964f0a51d06bd26119b7f20dad80a555c2bf2
Fejes Bence <febuabi@INF.ELTE.HU>
2016-10-19 11:48:39 +0200

    gyak6

--- app/Http/Controllers/RecipeController.js
+++ app/Http/Controllers/RecipeController.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Category = use('App/Model/Category')
+const Recipe = use('App/Model/Recipe')
+const Validator = use('Validator')
 
 class RecipeController {
     * main (req, res) {
@@ -21,6 +23,50 @@ class RecipeController {
 
         yield res.sendView('recipeCreate', {
             categories: categories.toJSON()
+        })
+    }
+
+    * doCreate (req, res) {
+        // 1. input
+        const recipeData = req.all()
+
+        // 2. validáció
+        const rules = {
+            'name': 'required|min:3',
+            'ingredients': 'required',
+            'instructions': 'required',
+        }
+
+        const validation = yield Validator.validateAll(recipeData, rules)
+        if (validation.fails()) {
+            yield req
+                .withAll()
+                .andWith({ errors: validation.messages() })
+                .flash()
+
+            res.redirect('/recipe/create')
+            return
+        }
+
+        // TODO: check category
+
+        const recipe = new Recipe()
+        recipe.name = recipeData.name
+        recipe.user_id = 1
+        recipe.category_id = recipeData.category
+        recipe.ingredients = recipeData.ingredients
+        recipe.instructions = recipeData.instructions
+
+        yield recipe.save()
+
+        res.redirect(`/recipe/${recipe.id}`)
+    } 
+
+    * show (req, res) {
+        const recipe = yield Recipe.find(req.param('id'))
+
+        yield res.sendView('recipe', {
+            recipe: recipe.toJSON()
         })
     }
 }
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -4,3 +4,6 @@ const Route = use('Route')
 
 Route.get('/', 'RecipeController.main')
 Route.get('/recipe/create', 'RecipeController.create')
+Route.post('/recipe/create', 'RecipeController.doCreate')
+
+Route.get('/recipe/:id', 'RecipeController.show')
